c59cf06b455be754db75c525439ee9085df80c19
jonabari <barcelo.jonathan@gmail.com>
2019-01-26 20:47:47 -0600

    Updates

---
+++ precache-manifest.8235104fd6f207546bbed2af916befc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7903b818f8256fcd3e0",
+    "url": "/bearl.ly/static/js/runtime~main.a7903b81.js"
+  },
+  {
+    "revision": "9683d24c3c2eb7ca0dd6",
+    "url": "/bearl.ly/static/js/main.9683d24c.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/bearl.ly/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "9683d24c3c2eb7ca0dd6",
+    "url": "/bearl.ly/static/css/main.5d8d3d7d.chunk.css"
+  },
+  {
+    "revision": "f597993bf311e22220cc9e6f444e141b",
+    "url": "/bearl.ly/index.html"
+  }
+];
